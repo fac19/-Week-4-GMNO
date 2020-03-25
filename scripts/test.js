2bd1c785b7526d77bf0cc946df31561aeb441667
Alexreid95 <alexreid95@gmail.com>
2020-03-25 15:48:32 +0000

    Create first test

---
+++ scripts/test.js
@@ -0,0 +1,15 @@
+const supertest = require("supertest");
+const router = require("./router");
+// const test = require("tape")
+
+test("Home route returns a status code of 200", t => {
+    supertest(router)
+        .get("/")
+        .expect(200)
+        .expect("content-type", "text/html")
+        .end((err, res) => {
+            t.error(err);
+            t.equal(res.text, "<h1>Hello, this was a success</h1>");
+            t.end();
+        });
+});
