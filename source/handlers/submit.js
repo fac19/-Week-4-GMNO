54a294f02af6000bf71611830da8cae19bd2adbf
Ina Yoon <itsina96@gmail.com>
2020-03-26 16:33:23 +0000

    make posts Relates #1,2
    Co-authored-by: Alexreid95 <alexreid95@gmail.com>
    Co-authored-by: jamesj-0 <jamesjhardy95@gmail.com>
    Co-authored-by: vatskan <vatskan@gmail.com>

--- source/handlers/submit.js
+++ source/handlers/submit.js
@@ -1,3 +1,9 @@
+const templates = require("../temp")
+const { parse } = require('querystring');
+// Global Arrays
+let blogPostArr = []; 
+let submitHTML = [];
+
 function submitHandler(request, response) {
     let bodyContent = '';
 
@@ -6,22 +12,25 @@ function submitHandler(request, response) {
     })
 
     request.on("end", () => {
-        console.log(bodyContent);
-        // const bodyData = new URLSearchParams(bodyContent);
-        // const name = bodyData.get("user-name");
-        // const title = bodyData.get("title");
-        // const text = bodyData.get("textBox");
+        const blogEntryObj = parse(bodyContent);
+        blogPostArr.push(blogEntryObj); 
 
         response.writeHead(200, {
             "content-type": "text/html"
         });
-        response.end(`<h1>hi</h1>`)
+        
+        // only print the last element of the array 
+        let blogPostArrLast = blogPostArr[blogPostArr.length-1];
+        
+        submitHTML = templates.submit(blogPostArrLast['title'], blogPostArrLast["user-name"], blogPostArrLast['textBox']);
+        console.log(blogPostArr); 
+        response.end(submitHTML);
     })
 
     response.on("error", error => {
         console.log(error);
         response.writeHead(500, {
-            "content-type": text / html
+            "content-type": "text/html"
         });
         const html = `<h1>Wow that's really broken huh</h1>`;
         response.end(html);
--- source/temp.js
+++ source/temp.js
@@ -1,16 +1,15 @@
 function sharedLayout(bodyContent) {
     return `<!DOCTYPE html>
-    <html lang="en">
-    
-    <head>
+    <html lang="en">    
+        <head>
         <meta charset="UTF-8">
         <meta name="viewport" content="width=device-width, initial-scale=1.0">
         <link rel="stylesheet" href="style.css">
         <title>Blog</title>
-    </head>
-    <body>
+        </head>
+        <body>
         ${bodyContent}
-    </body>
+        </body>
     </html>
     `
 }
@@ -19,23 +18,38 @@ function home() {
     return sharedLayout(`    
     <section class="form-section">
     <form id="form" action="/blog-update" method="POST">
-        <label for="title">Title:</label>
-        <input type="text" id="title" name="title" placeholder="Type your title">
-        <label for="user-name">Your Name:</label>
-        <input type="text" id="user-name" name="user-name" placeholder="Type your user name">
-        <label for="textBox">Enter your blog post below.</label>
-        <textarea form="form" id="textBox" name="textBox" placeholder="Let your imagination run free..."></textarea>
-        <button type="submit">Submit</button>
+    <label for="title">Title:</label>
+    <input type="text" id="title" name="title" placeholder="Type your title">
+    <label for="user-name">Your Name:</label>
+    <input type="text" id="user-name" name="user-name" placeholder="Type your user name">
+    <label for="textBox">Enter your blog post below.</label>
+    <textarea form="form" id="textBox" name="textBox" placeholder="Let your imagination run free..."></textarea>
+    <button type="submit">Submit</button>
     </form>
-</section>`);
+    </section>`);
 }
 
-function submit() {
 
+let str = ''; 
+
+function submit(title, name, textBox) {
+    let localStr = `
+    <section class="display-post">
+        <article>
+            <h2>${title}</h2>
+            <h3>${name}</h3>
+            <p>${textBox}</p>
+        </article>
+    </section>
+    `;
+
+    str += localStr;
+
+    return sharedLayout(str);
 }
 
 function missing() {
-
+    
 }
 
 module.exports = {
