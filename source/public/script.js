cb40ed3bda68fe540ecf90d87734be303f99277e
James-Hardy <jamesjhardy95@gmail.com>
2020-03-27 08:42:47 +0000

    add delete post

--- source/public/script.js
+++ source/public/script.js
@@ -1,12 +1,25 @@
-import { format } from "url";
+// import { format } from "url";
 
-// const form = document.querySelector('')
 const message = document.querySelector("#textBox");
 
 
-document.querySelector("form").onkeypress = function(e) {
+document.getElementsByClassName("form").onkeypress = function(e) {
     let key = e.charCode || e.keyCode; // using charCode or keyCode for cross-browser support
     if (key == 13 && e.target !== message) {
       e.preventDefault(); // do not disable default enter behaviour on message textarea
     }
   };
+
+
+var buttons = document.querySelectorAll('.post-remove__button');
+
+for(var i=0; i < buttons.length; i++){
+  buttons[i].addEventListener('click', function () {
+    console.log(this.parentNode); 
+    this.parentNode.remove();
+    }, false);
+}
+
+
+
+  // <input class="form__input" type="text" id="title" name="title" placeholder="Type your title" required>
